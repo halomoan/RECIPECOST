81be0bbd17a5e0f9d8e0ef7266ce246d637dd2cb
Halomoan <halomoan@gmail.com>
2021-06-26 14:56:15 +0800

    home commit

--- webapp/pages/controller/Recipes.controller.js
+++ webapp/pages/controller/Recipes.controller.js
@@ -6,9 +6,10 @@ sap.ui.define([
 	"sap/ui/core/message/Message",
 	"sap/ui/core/library",
 	'sap/ui/model/Filter',
-	'sap/ui/model/FilterOperator'
-	
-], function(BaseController,JSONModel,MessageBox,MessageToast,Message,library,Filter,FilterOperator) {
+	'sap/ui/model/FilterOperator',
+	'sap/ui/model/Sorter'
+
+], function(BaseController, JSONModel, MessageBox, MessageToast, Message, library, Filter, FilterOperator,Sorter) {
 	"use strict";
 	var _oBundle;
 	// shortcut for sap.ui.core.ValueState
@@ -19,116 +20,169 @@ sap.ui.define([
 	return BaseController.extend("halo.sap.mm.RECIPECOST.pages.controller.Recipes", {
 
 		_formFragments: {},
-		
+
 		onInit: function() {
-			
+
 			var oView = this.getView();
-	
+
 			var oFormData = {
-				"Name" : "",
+				"Name": "",
 				"GroupID": "",
 				"LocationID": "",
 				"Quantity": 1.0
 			};
-			
+
+			this.PurchOrgID = "C103";
+			this.PlantID = "PPHS";
+
 			var oFormModel = new JSONModel(oFormData);
 			oFormModel.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
 			this.setModel(oFormModel, "form");
-			
-			
+
 			// set message model
 			var oMessageManager = sap.ui.getCore().getMessageManager();
 			oView.setModel(oMessageManager.getMessageModel(), "message");
-			oMessageManager.registerObject(oView, true);	
-				
+			oMessageManager.registerObject(oView, true);
+
 			this.getOwnerComponent().getModel().metadataLoaded().then(function() {
-				_oBundle = this.getResourceBundle();
-				
+				this._init();
+
 			}.bind(this));
-			
-			
-			this.PurchOrgID = "C103";
-			this.PlantID = "PPHS";
+
 		},
 		
-		onAddRecipe: function(){
+		_init: function(){
 			
+			var oView = this.getView();
+			_oBundle = this.getResourceBundle();
+
+			var oGroupList = oView.byId("byGroup");
+
+			oGroupList.bindAggregation("items", {
+				path: "/RecipeGroupSet",
+				filters: [new Filter("Werks", FilterOperator.EQ, this.PlantID)],
+				sorter: new Sorter({path: 'Text', descending: true}),
+				template: new sap.ui.core.ListItem({
+					text: "{Text}",
+					key: "{Groupid}"
+				})
+			});
 			
+			var oTable = oView.byId("recipeTable");
+		
+			var oTemplate = oTable.getBindingInfo("items").template;
 			
-			this.showFormDialogFragment(this.getView(), this._formFragments, "halo.sap.mm.RECIPECOST.fragments.RecipeForm", this);
-			
-			// Filter Location List
-			var oLocList  = this.getView().byId("location");
-			var oLocBinding = oLocList.getBinding("items");
-			oLocBinding.filter([new Filter("Werks", FilterOperator.EQ, this.PlantID)], "Application");
-			
-			// Filter Location List
-			var oGroupList  = this.getView().byId("group");
-			var oGroupBinding = oGroupList.getBinding("items");
-			oGroupBinding.filter([new Filter("Werks", FilterOperator.EQ, this.PlantID)], "Application");
+			oTable.bindAggregation("items", {
+				path: "/RecipeSet",
+				filters: [new Filter("Werks", FilterOperator.EQ, this.PlantID)],
+				sorter: new Sorter({path: 'Name', descending: true}),
+				template: oTemplate
+			});
 			
 		},
-		onSaveRecipe: function(){
+
+		onAddRecipe: function() {
+
+			this.showFormDialogFragment(this.getView(), this._formFragments, "halo.sap.mm.RECIPECOST.fragments.RecipeForm", this);
+
+			// Filter Location List
+			var oLocList = this.getView().byId("location");
+			var oLocBinding = oLocList.getBinding("items");
+			oLocBinding.filter([new Filter("Werks", FilterOperator.EQ, this.PlantID)], "Application");
+
+			// Filter Location List
+			var oGroupList = this.getView().byId("group");
+			oGroupList.bindAggregation("items", {
+					path: "/RecipeGroupSet",
+					filters: [new Filter("Werks", FilterOperator.EQ, this.PlantID)],
+					sorter: new Sorter({path: 'Text', descending: true}),
+					template: new sap.ui.core.ListItem({
+						text: "{Text}",
+						key: "{Groupid}"
+					})
+			});
+			//var oGroupBinding = oGroupList.getBinding("items");
+			//oGroupBinding.filter([new Filter("Werks", FilterOperator.EQ, this.PlantID)], "Application");
+
+		},
+		
+		onSaveRecipe: function() {
 			var oFormData = this.getView().getModel("form").getData();
 			var oThis = this;
-			
-			
+
 			if (this._validateRecipe(oFormData)) {
-				
+
 				MessageBox.confirm(_oBundle.getText("msgCfrmSaveRecipe"), {
-						actions: ["Save", MessageBox.Action.CANCEL],
-						emphasizedAction: "CANCEL",
-						onClose: function(sAction) {
-							if (sAction === 'Save') {
-								
-								oThis._saveRecipe(oFormData);
-								oThis.byId("addRecipeDialog").close();
-							} else {
-								oThis.byId("addRecipeDialog").close();
-							}
+					actions: ["Save", MessageBox.Action.CANCEL],
+					emphasizedAction: "CANCEL",
+					onClose: function(sAction) {
+						if (sAction === 'Save') {
+
+							oThis._saveRecipe(oFormData);
+							oThis.byId("addRecipeDialog").close();
+						} else {
+							oThis.byId("addRecipeDialog").close();
 						}
-						
+					}
+
 				});
-				
+
 			} else {
 				MessageToast.show(_oBundle.getText("msgErrFormError"));
 			}
 		},
 		
-		onMessagePopoverPress : function (oEvent) {
-			var oSource = oEvent.getSource();
+		onGroupChanged: function(oEvent){
+			var selKey = oEvent.getParameter("selectedItem").getKey();
 			
-			this.showPopOverFragment(this.getView(), oSource, this._formFragments, "halo.sap.mm.RECIPECOST.fragments.MessagePopover", this );         
+			var aFilter = [];
+			
+			aFilter.push(new Filter("Werks", FilterOperator.EQ,  this.PlantID));
+			aFilter.push(new Filter("GroupID", FilterOperator.EQ,  selKey));
+			
+			var oList = this.byId("recipeTable");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+			
+			
+
 		},
 		
-		onSelectImage: function(oEvent){
+		onMessagePopoverPress: function(oEvent) {
+			var oSource = oEvent.getSource();
+
+			this.showPopOverFragment(this.getView(), oSource, this._formFragments, "halo.sap.mm.RECIPECOST.fragments.MessagePopover", this);
+		},
+
+		onSelectImage: function(oEvent) {
 			var oSource = oEvent.getSource();
 			var oParent = oSource.getParent();
 			var sPath = oParent.getBindingContext().getPath();
-		
-			this.showPopOverFragment(this.getView(), oSource, this._formFragments, "halo.sap.mm.RECIPECOST.fragments.ImageUploadPopover", this ); 
-			
-		
-			this.byId("ImagePopover").bindElement({path: sPath});
-		
+
+			this.showPopOverFragment(this.getView(), oSource, this._formFragments, "halo.sap.mm.RECIPECOST.fragments.ImageUploadPopover", this);
+
+			this.byId("ImagePopover").bindElement({
+				path: sPath
+			});
+
 		},
-		
-		onUploadImage:function(){
+
+		onUploadImage: function() {
 			var oUploader = this.byId("ImageUploader");
-			
+
 			var sPath = this.byId("ImagePopover").getBindingContext().getPath();
-			
+
 			sPath = sPath.replace("RecipeSet", "RecipeVersionSet") + "/Photo";
-			
+
 			//console.log(sPath);
-			
+
 			if (!oUploader.getValue()) {
 				MessageToast.show("Choose a file first");
 				return;
 			}
-			
+
 			oUploader.setUploadUrl(sPath);
-			
+
 			oUploader.checkFileReadable().then(function() {
 				oUploader.upload();
 			}, function(error) {
@@ -137,19 +191,18 @@ sap.ui.define([
 				oUploader.clear();
 			});
 		},
-		onImgUploaderClose: function(){
-			var oPopOver = this.getFragmentByName(this._formFragments,"halo.sap.mm.RECIPECOST.fragments.ImageUploadPopover");
+		onImgUploaderClose: function() {
+			var oPopOver = this.getFragmentByName(this._formFragments, "halo.sap.mm.RECIPECOST.fragments.ImageUploadPopover");
 			oPopOver.close();
 		},
-		_saveRecipe: function(oFormData){
+		_saveRecipe: function(oFormData) {
 			var oModel = this.getModel();
 			var oData = {
-				"Werks" : this.PlantID,
+				"Werks": this.PlantID,
 				"Name": oFormData.Name,
 				"GroupID": oFormData.GroupID,
 				"LocationID": oFormData.LocationID,
 				"Quantity": "" + oFormData.Quantity
-				
 
 			};
 
@@ -164,17 +217,16 @@ sap.ui.define([
 			});
 
 		},
-		_validateRecipe: function(oFormData){
-		
+		_validateRecipe: function(oFormData) {
+
 			var oMessage;
 			var status = true;
-		
-			
+
 			sap.ui.getCore().getMessageManager().removeAllMessages();
-			
+
 			if (oFormData.Name.length < 5) {
-					status = false;
-					oMessage = new Message({
+				status = false;
+				oMessage = new Message({
 					message: "Empty Is not allowed. Minimum 5 characters",
 					type: MessageType.Error,
 					target: "/Name",
@@ -182,10 +234,10 @@ sap.ui.define([
 				});
 				sap.ui.getCore().getMessageManager().addMessages(oMessage);
 			}
-			
+
 			if (oFormData.GroupID.length < 1) {
 				status = false;
-				
+
 				oMessage = new Message({
 					message: "Empty Is not allowed",
 					type: MessageType.Error,
@@ -194,10 +246,10 @@ sap.ui.define([
 				});
 				sap.ui.getCore().getMessageManager().addMessages(oMessage);
 			}
-			
+
 			if (oFormData.LocationID.length < 1) {
 				status = false;
-				
+
 				oMessage = new Message({
 					message: "Empty Is not allowed",
 					type: MessageType.Error,
@@ -206,17 +258,15 @@ sap.ui.define([
 				});
 				sap.ui.getCore().getMessageManager().addMessages(oMessage);
 			}
-			
-			
-			return status;	
+
+			return status;
 		},
-		
-	
-		onCancelAddRecipe: function(){
+
+		onCancelAddRecipe: function() {
 			this.byId("addRecipeDialog").close();
 		},
 		onExit: function() {
-		
+
 			this.removeFragment(this._formFragments);
 		}
 
